2cb7e1f633be862c2526845e24fe2dad77423d2d
Sreekar Pothuri <sreekar.pothuri1998@gmail.com>
2020-06-27 12:06:08 +0530

    UC-3:Added

--- src/com/empwage/service/EmployeeWage.java
+++ src/com/empwage/service/EmployeeWage.java
@@ -1,19 +1,30 @@
 public class EmployeeWage{
 	public static void main(String args[]){
+		int is_full_time=1;
+		int is_part_time=2;
+
 		int WagePerHr = 20;
-		int HrsPerDay = 8;
+		int HrsPerDay;
 		int DailyWageSalary;
+
 		System.out.println("*****Welcome to Employee Wage Computation Program*****");
-		double EmpCheck = Math.floor(Math.random() * 10) % 2;
-		if(EmpCheck == 1)
-		{
-			System.out.println("Employee is present");
-			DailyWageSalary = WagePerHr * HrsPerDay;
-			System.out.println("Salary : "+DailyWageSalary);
+
+		double EmpCheck = Math.floor(Math.random() * 10) % 3;
+
+		if(EmpCheck == is_full_time){
+			System.out.println("Full Time Employee is Present");
+			HrsPerDay=8;
+		}
+		else if(EmpCheck == is_part_time){
+			System.out.println("Part Time Employee is Present");
+			HrsPerDay=4;
 		}
 		else
 		{
 			System.out.println("Employee is absent");
+			HrsPerDay=0;
 		}
+		DailyWageSalary = WagePerHr * HrsPerDay;
+		System.out.println("Salary : "+DailyWageSalary);
 	}
 }
