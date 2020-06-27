96ca220ec2a4ffa3222af18ec44b27073385e59d
Sreekar Pothuri <sreekar.pothuri1998@gmail.com>
2020-06-27 11:56:12 +0530

    UC-2:Added

--- src/com/empwage/service/EmployeeWage.java
+++ src/com/empwage/service/EmployeeWage.java
@@ -1,10 +1,19 @@
 public class EmployeeWage{
 	public static void main(String args[]){
+		int WagePerHr = 20;
+		int HrsPerDay = 8;
+		int DailyWageSalary;
 		System.out.println("*****Welcome to Employee Wage Computation Program*****");
 		double EmpCheck = Math.floor(Math.random() * 10) % 2;
 		if(EmpCheck == 1)
+		{
 			System.out.println("Employee is present");
+			DailyWageSalary = WagePerHr * HrsPerDay;
+			System.out.println("Salary : "+DailyWageSalary);
+		}
 		else
+		{
 			System.out.println("Employee is absent");
+		}
 	}
 }
