f1ba23fc62c86b5c9a933de37ac9c1d1390c0781
Sreekar Pothuri <sreekar.pothuri1998@gmail.com>
2020-06-27 14:11:21 +0530

    UC-8:Added

--- src/com/empwage/service/EmployeeWage.java
+++ src/com/empwage/service/EmployeeWage.java
@@ -1,46 +1,46 @@
 public class EmployeeWage{
 
-	public static final int WagePerHr = 20;
-	public static final int WorkingDaysPerMonth = 20;
-	public static final int WorkingHrsPerMonth = 100;
-	static int Days=1;
-	static int HrsPerDay;
-
-	public static void EmpWage(){
-		double EmpCheck = Math.floor(Math.random() * 10) % 3;
-		int CheckEmp =(int) EmpCheck;//Type casting double to int for Emp attendance check
-
-		switch(CheckEmp){
-			case 1:
-				System.out.print("Full Time Employee and he is Present");
-				HrsPerDay=8;
-				break;
-			case 2:
-				System.out.print("Part Time Employee and he is Present");
-				HrsPerDay=4;
-				break;
-			default:
-				System.out.print("Employee is absent\t"+"\t");
-				HrsPerDay=0;
-				break;
-		}
-	}
-
-	public static void main(String args[]){
-		int DailyWage;
-		int MonthlyWage=0;
+	public static int computeEmpWage(String company, int WagePerHr, int WorkingDaysPerMonth, int WorkingHrsPerMonth){
+		int HrsPerDay=0;
 		int totalWorkingHrs=0;
-
-		System.out.println("*****Welcome to Employee Wage Computation Program*****");
+		int Days=1;
+		int MonthlyWage=0;
+		int DailyWage;
 
 		while(Days<=WorkingDaysPerMonth && totalWorkingHrs<=WorkingHrsPerMonth){
-			EmpWage();
+			double EmpCheck = Math.floor(Math.random() * 10) % 3;
+			int CheckEmp =(int) EmpCheck;//Type casting double to int for Emp attendance check
+
+			switch(CheckEmp){
+				case 1:
+					System.out.print("Full Time Employee and he is Present");
+					HrsPerDay=8;
+					break;
+				case 2:
+					System.out.print("Part Time Employee and he is Present");
+					HrsPerDay=4;
+					break;
+				default:
+					System.out.print("Employee is absent\t"+"\t");
+					HrsPerDay=0;
+					break;
+			}
+
 			DailyWage = WagePerHr * HrsPerDay;
 			System.out.println("\t"+"Day#"+Days+"\t"+"Salary :"+DailyWage);
 			MonthlyWage = MonthlyWage + DailyWage;
 			totalWorkingHrs = totalWorkingHrs + HrsPerDay;
 			Days++;
 		}
-		System.out.println("Total Working Hours:"+totalWorkingHrs+"\tTotal monthly wage: "+MonthlyWage);
+			System.out.println("Emp Wage for Company:"+company+"\tTotal Working Hours:"+totalWorkingHrs+"\tTotal Monthly Salary:"+MonthlyWage);
+			return MonthlyWage;
+	}
+	public static void main(String args[]){
+		System.out.println("*****Welcome to Employee Wage Computation Program*****");
+		computeEmpWage("Amazon",10,2,10);
+		computeEmpWage("Netflix",20,4,20);
+		computeEmpWage("Zee5",30,6,30);
+		computeEmpWage("SonyLIV",40,8,40);
+		computeEmpWage("Aha",50,10,50);
 	}
 }
