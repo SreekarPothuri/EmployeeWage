2fc138661ce8b54dcdc1a6c00ab1ee2d462d964c
Sreekar Pothuri <sreekar.pothuri1998@gmail.com>
2020-06-27 12:47:38 +0530

    UC-5:Added

--- src/com/empwage/service/EmployeeWage.java
+++ src/com/empwage/service/EmployeeWage.java
@@ -3,28 +3,37 @@ public class EmployeeWage{
 
 		int WagePerHr = 20;
 		int HrsPerDay;
-		int DailyWageSalary;
+		int DailyWage;
+
+		int WorkingDaysPerMonth=20;
+		int Days;
+		int MonthlyWage=0;
 
 		System.out.println("*****Welcome to Employee Wage Computation Program*****");
 
-		double EmpCheck = Math.floor(Math.random() * 10) % 3;
-		int CheckEmp =(int) EmpCheck;//Type casting double to int for Emp attendance check
+		for(Days=1; Days<=WorkingDaysPerMonth; Days++){
 
-		switch(CheckEmp){
-			case 1:
-				System.out.println("Full Time Employee and he is Present");
-				HrsPerDay=8;
-				break;
-			case 2:
-				System.out.println("Part Time Employee and he is Present");
-				HrsPerDay=4;
-				break;
-			default:
-				System.out.println("Employee is absent");
-				HrsPerDay=0;
-				break;
+			double EmpCheck = Math.floor(Math.random() * 10) % 3;
+			int CheckEmp =(int) EmpCheck;//Type casting double to int for Emp attendance check
+
+			switch(CheckEmp){
+				case 1:
+					System.out.print("Full Time Employee and he is Present");
+					HrsPerDay=8;
+					break;
+				case 2:
+					System.out.print("Part Time Employee and he is Present");
+					HrsPerDay=4;
+					break;
+				default:
+					System.out.print("Employee is absent\t"+"\t");
+					HrsPerDay=0;
+					break;
+			}
+			DailyWage = WagePerHr * HrsPerDay;
+			System.out.println("\t"+"Day#"+Days+"\t"+"Salary :"+DailyWage);
+			MonthlyWage = MonthlyWage + DailyWage;
 		}
-		DailyWageSalary = WagePerHr * HrsPerDay;
-		System.out.println("Salary : "+DailyWageSalary);
+		System.out.println("Total monthly wage: "+MonthlyWage);
 	}
 }
