5cbbcbd09631951c921f5d9716ee61fbdf434d8b
Sreekar Pothuri <sreekar.pothuri1998@gmail.com>
2020-06-28 13:15:24 +0530

    UC-11:Added

--- src/com/empwage/service/EmployeeWage.java
+++ src/com/empwage/service/EmployeeWage.java
@@ -1,24 +1,32 @@
+package com.empwage.service;
+import com.empwage.model.CompanyEmpWage;
 public class EmployeeWage{
-	private final String company;
-	private final int WagePerHr;
-	private final int WorkingDaysPerMonth;
-	private final int WorkingHrsPerMonth;
-	private int MonthlyWage=0;
+	private static int numOfCompany = 0;
+	private static CompanyEmpWage[] companyEmpWageArray;
 
-	public EmployeeWage(String company, int WagePerHr, int WorkingDaysPerMonth, int WorkingHrsPerMonth){
-		this.company = company;
-		this.WagePerHr = WagePerHr;
-		this.WorkingDaysPerMonth = WorkingDaysPerMonth;
-		this.WorkingHrsPerMonth = WorkingHrsPerMonth;
+	public EmployeeWage(){
+		companyEmpWageArray = new CompanyEmpWage[5];
 	}
 
-	public void computeEmpWage(){
+	public void addCompanyEmpWage(String company,int WagePerHr, int WorkingDaysPerMonth, int WorkingHrsPerMonth){
+		companyEmpWageArray[numOfCompany] = new CompanyEmpWage(company,WagePerHr,WorkingDaysPerMonth,WorkingHrsPerMonth);
+		numOfCompany++;
+	}
+
+	private static void computeEmpWage(){
+		for(int i=0; i<numOfCompany; i++){
+			companyEmpWageArray[i].setTotalEmpWage(computeEmpWage(companyEmpWageArray[i]));
+			System.out.println(companyEmpWageArray[i]);
+		}
+	}
+
+	public static int computeEmpWage(CompanyEmpWage companyEmpWage){
 		int HrsPerDay=0;
 		int DailyWage;
 		int totalWorkingHrs=0;
 		int Days=1;
 
-		while(Days<=WorkingDaysPerMonth && totalWorkingHrs<=WorkingHrsPerMonth){
+		while(Days<=companyEmpWage.WorkingDaysPerMonth && totalWorkingHrs<=companyEmpWage.WorkingHrsPerMonth){
 			double EmpCheck = Math.floor(Math.random() * 10) % 3;
 			int CheckEmp =(int) EmpCheck;//Type casting double to int for Emp attendance check
 
@@ -36,32 +44,21 @@ public class EmployeeWage{
 					HrsPerDay=0;
 					break;
 			}
-
-			DailyWage = WagePerHr * HrsPerDay;
-			System.out.println("\t"+"Day#"+Days+"\t"+"Salary :"+DailyWage);
-			MonthlyWage = MonthlyWage + DailyWage;
 			totalWorkingHrs = totalWorkingHrs + HrsPerDay;
+			System.out.println("\tEmpHr: "+HrsPerDay);
 			Days++;
 		}
-			 System.out.println("Emp Wage for Company:"+company+"\tTotal Working Hours:"+totalWorkingHrs+"\tTotal Monthly Salary:"+MonthlyWage);
+			return totalWorkingHrs * companyEmpWage.WagePerHr;
 	}
+
 	public static void main(String args[]){
 		System.out.println("*****Welcome to Employee Wage Computation Program*****");
-		EmployeeWage Amazon = new EmployeeWage("Amazon",10,2,10);
-		EmployeeWage Netflix = new EmployeeWage("Netflix",20,4,20);
-		EmployeeWage Zee5 = new EmployeeWage("Zee5",30,6,30);
-		EmployeeWage SonyLIV = new EmployeeWage("SonyLIV",40,8,40);
-		EmployeeWage Aha = new EmployeeWage("Aha",50,10,50);
-
-		Amazon.computeEmpWage();
-		System.out.println(Amazon);
-		Netflix.computeEmpWage();
-		System.out.println(Netflix);
-		Zee5.computeEmpWage();
-		System.out.println(Zee5);
-		SonyLIV.computeEmpWage();
-		System.out.println(SonyLIV);
-		Aha.computeEmpWage();
-		System.out.println(Aha);
+		EmployeeWage empWage = new EmployeeWage();
+		empWage.addCompanyEmpWage("Amazon",10,2,10);
+		empWage.addCompanyEmpWage("Netflix",20,4,20);
+		empWage.addCompanyEmpWage("Zee5",30,6,30);
+		empWage.addCompanyEmpWage("SonyLIV",40,8,40);
+		empWage.addCompanyEmpWage("Aha",50,10,50);
+		EmployeeWage.computeEmpWage();
 	}
 }
