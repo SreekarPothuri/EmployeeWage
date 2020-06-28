2358c6d4d2cd501750aad9c94abe07670f7e3b66
Sreekar Pothuri <sreekar.pothuri1998@gmail.com>
2020-06-28 22:51:41 +0530

    UC-12,13,14:Added

--- src/com/empwage/model/CompanyEmpWage.java
+++ src/com/empwage/model/CompanyEmpWage.java
@@ -1,10 +1,10 @@
 package com.empwage.model;
 
 public class CompanyEmpWage{
-	public String company;
-	public int WagePerHr;
-	public int WorkingDaysPerMonth;
-	public int WorkingHrsPerMonth;
+	public final String company;
+	public final int WagePerHr;
+	public final int WorkingDaysPerMonth;
+	public final int WorkingHrsPerMonth;
 	public int MonthlyWage;
 
 	public CompanyEmpWage(String company, int WagePerHr, int WorkingDaysPerMonth, int WorkingHrsPerMonth){
@@ -12,12 +12,14 @@ public class CompanyEmpWage{
 		this.WagePerHr = WagePerHr;
 		this.WorkingDaysPerMonth = WorkingDaysPerMonth;
 		this.WorkingHrsPerMonth = WorkingHrsPerMonth;
+		MonthlyWage = 0;
 	}
 
 	public void setTotalEmpWage(int MonthlyWage){
 		this.MonthlyWage = MonthlyWage;
 	}
 
+	@Override
 	public String toString(){
 		return "Total Employee wage for company: "+company+" is:"+MonthlyWage;
 	}
